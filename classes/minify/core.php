870a507c895d8a08564b96570b6f9095cfc5828e
Ryder Ross <ryross@gmail.com>
2010-03-31 12:32:45 -0700

    cleaned up the code in core.php. other files must be CRLF conversions

--- classes/minify/core.php
+++ classes/minify/core.php
@@ -9,24 +9,24 @@
  *	$this->template->jsFiles = Minify::factory('js')->minify( $this->template->jsFiles, $build );
  *	$this->template->cssFiles = Minify::factory('css')->minify( $this->template->cssFiles, $build );
  *
- *  View:
- 	foreach ($cssFiles as $css) {
-		if ( ! Kohana::config('minify.enabled') || $debug ) 
-			$js = "views/css/{$css}?{$build}";
-		echo HTML::style($css),"\n";
-	}
-	// application js files
-	foreach ($jsFiles as $js) { 
-		if ( ! Kohana::config('minify.enabled') || $debug ) 
-			$js = "views/jscript/{$js}?{$build}";
-		echo HTML::script($js),"\n";
-	}
+ *  View: 
+ * 	foreach ($cssFiles as $css) {
+ *		if ( ! Kohana::config('minify.enabled') || $debug ) 
+ * 			$js = "views/css/{$css}?{$build}";
+ * 		echo HTML::style($css),"\n";
+ * 	}
+ * 	// application js files
+ * 	foreach ($jsFiles as $js) { 
+ * 		if ( ! Kohana::config('minify.enabled') || $debug ) 
+ * 			$js = "views/jscript/{$js}?{$build}";
+ * 		echo HTML::script($js),"\n";
+ * 	}
  */
 class Minify_Core {
 	
 	protected $type;
 	protected $file;
-	protected $input       = '';	
+	protected $input       = '';
   	protected $inputLength = 0;
 
 	public function __construct( $type ) {
@@ -39,43 +39,58 @@ class Minify_Core {
 	}
 
 	// Dateien zusammenfassen, komprimieren und im media verzeichnis ablegen
-	public function minify( $files , $build='' ) {
-		if ( Kohana::config('minify.enabled', false ) ) {
-			$name = md5( json_encode( $files ) );
+	public function minify( $files , $build='' )
+	{
+		if (Kohana::config('minify.enabled', false))
+		{
+			$name = md5(json_encode($files));
 			$outfile = Kohana::config('minify.path.media').$name.$build.'.'.$this->type;
-			if ( ! is_file( $outfile ) ) {
-				if (! is_array( $files ))
-					$files = array( $files ); 	
+			if ( ! is_file($outfile))
+			{
+				if ( ! is_array($files))
+				{
+					$files = array( $files );
+				}
 				$output = ''; 
-				foreach( $files as $file ) {
+				foreach($files as $file)
+				{
 					$this->file = Kohana::config('minify.path.'.$this->type).$file;
-					if ( is_file($this->file) ) {
-						$this->set( file_get_contents( $this->file ) );
+					if (is_file($this->file))
+					{
+						$this->set(file_get_contents($this->file));
 						$output .= $this->min() . "\r\n";
 					}
 				}
+				
 				// write minified file 
-				$f = fopen( $outfile , 'w' );
-				if ( $f ) {
-					fwrite( $f, $output );
-					fclose( $f );
+				$f = fopen($outfile, 'w');
+				if ($f)
+				{
+					fwrite($f, $output);
+					fclose($f);
 				}
 			}
-			return array ( $outfile );
+			return array($outfile);
 		}
 		else
+		{
 			return $files;
+		}
 	}
 
 	// text an minifier Übergeben (per member variable)
-	public function set($input) {
+	public function set($input)
+	{
 		$this->input       = str_replace("\r\n", "\n", $input);
-    	$this->inputLength = strlen($this->input);
-    	return $this;
-  	}
-
+		$this->inputLength = strlen($this->input);
+		return $this;
+	}
+	
 	// text komprimieren (abgeleitete Klasse)
-	public function min() { return $this->input; }
+	public function min()
+	{
+		return $this->input;
+	}
 	
 	
 }
--- classes/minify/css.php
+++ classes/minify/css.php
@@ -23,59 +23,60 @@
  */
 
 
-class Minify_CSS  extends Minify_Core {
-	
-    public function min() {
-        $css = $this->input;
-        $options = "";
-        $options = ($options == "") ? array() : (is_array($options) ? $options : explode(",", $options));
-        // Remove comments
-        $css = preg_replace("/\/\*[\d\D]*?\*\/|\t+/", " ", $css);
-        // Replace CR, LF and TAB to spaces
-        $css = str_replace(array("\n", "\r", "\t"), " ", $css);
-        // Replace multiple to single space
-        $css = preg_replace("/\s\s+/", " ", $css);
-        // Remove unneeded spaces
-        $css = preg_replace("/\s*({|}|\[|\]|=|~|\+|>|\||;|:|,)\s*/", "$1", $css);
-        if (in_array("remove-last-semicolon", $options)) {
-            // Removes the last semicolon of every style definition
-            $css = str_replace(";}", "}", $css);
-        }
-        $css = trim($css);
-        
-        // prevent url
-        $path = url::base(FALSE);
-		if ( strrpos($this->file,'/') !== false )
-            $path .= substr($this->file,0,strrpos($this->file,'/')+1); 
-     	// Fix all paths within this CSS file, ignoring absolute paths.
-        $css = preg_replace('/url\(([\'"]?)(?![a-z]+:)/i', 'url(\1'. $path . '\2', $css);            	
-        
-        
-        return $css;
-    }
+class Minify_CSS  extends Minify_Core
+{
+	public function min()
+	{
+		$css = $this->input;
+		$options = "";
+		$options = ($options == "") ? array() : (is_array($options) ? $options : explode(",", $options));
+		// Remove comments
+		$css = preg_replace("/\/\*[\d\D]*?\*\/|\t+/", " ", $css);
+		// Replace CR, LF and TAB to spaces
+		$css = str_replace(array("\n", "\r", "\t"), " ", $css);
+		// Replace multiple to single space
+		$css = preg_replace("/\s\s+/", " ", $css);
+		// Remove unneeded spaces
+		$css = preg_replace("/\s*({|}|\[|\]|=|~|\+|>|\||;|:|,)\s*/", "$1", $css);
+		if (in_array("remove-last-semicolon", $options))
+		{
+			// Removes the last semicolon of every style definition
+			$css = str_replace(";}", "}", $css);
+		}
+		$css = trim($css);
+		
+		// prevent url
+		$path = url::base(FALSE);
+		if (strrpos($this->file,'/') !== FALSE)
+		{
+			$path .= substr($this->file, 0, strrpos($this->file,'/') + 1);
+		}
+		// Fix all paths within this CSS file, ignoring absolute paths.
+		//$css = preg_replace('/url\(([\'"]?)(?![a-z]+:)/i', 'url(\1'. $path . '\2', $css);
+		return $css;
+	}
 
-
-    public function cssmin_array_clean(array $array) {
-        $r = array();
-        $c = count($v);
-        if (cssmin_array_is_assoc($array))
-        {
-            foreach ($array as $key => $value)
-            {
-                $r[$key] = trim($value);
-            }
-        }
-        else
-        {
-            foreach ($array as $value)
-            {
-                if (trim($value) != "")
-                {
-                    $r[] = trim($value);
-                }
-            }
-        }
-        return $r;
-    }
-
+	public function cssmin_array_clean(array $array)
+	{
+		$r = array();
+		$c = count($v);
+		if (cssmin_array_is_assoc($array))
+		{
+			foreach ($array as $key => $value)
+			{
+				$r[$key] = trim($value);
+			}
+		}
+		else
+		{
+			foreach ($array as $value)
+			{
+				if (trim($value) != "")
+				{
+					$r[] = trim($value);
+				}
+			}
+		}
+		return $r;
+	}
 }
